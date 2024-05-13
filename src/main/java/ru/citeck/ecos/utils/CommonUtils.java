32ceea513b9c86aae30119ccf6ff08badd54a5ad
Dmitry Fedotov <dmitry.fedotov@citeck.ru>
2024-05-13 22:11:18 +0300

    Fix ECOS Form Duplicate Component Key inspection

---
+++ src/main/java/ru/citeck/ecos/utils/CommonUtils.java
@@ -0,0 +1,11 @@
+package ru.citeck.ecos.utils;
+
+import java.util.function.Function;
+
+public class CommonUtils {
+
+    public static <U> Function<Object, U> filterAndCast(Class<? extends U> clazz) {
+        return t -> clazz.isInstance(t) ? clazz.cast(t) : null;
+    }
+
+}
