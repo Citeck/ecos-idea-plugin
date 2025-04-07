0e29b1f11015690123c13f2c1cb88e7481a5a004
Roman Makarskiy <romanchabest55@gmail.com>
2025-04-07 15:57:25 +0300

    add template schema

--- src/main/kotlin/ru/citeck/idea/schema/CiteckJsonSchemaProviderFactory.kt
+++ src/main/kotlin/ru/citeck/idea/schema/CiteckJsonSchemaProviderFactory.kt
@@ -7,6 +7,7 @@ import ru.citeck.idea.schema.impl.ActionJsonSchemaProvider
 import ru.citeck.idea.schema.impl.AspectJsonSchemaProvider
 import ru.citeck.idea.schema.impl.BoardJsonSchemaProvider
 import ru.citeck.idea.schema.impl.BpmnKpiSettingsJsonSchemaProvider
+import ru.citeck.idea.schema.impl.BpmnTaskAttsSyncJsonSchemaProvider
 import ru.citeck.idea.schema.impl.EndpointJsonSchemaProvider
 import ru.citeck.idea.schema.impl.JournalJsonSchemaProvider
 import ru.citeck.idea.schema.impl.ModelTypeJsonSchemaProvider
@@ -26,6 +27,7 @@ class CiteckJsonSchemaProviderFactory : JsonSchemaProviderFactory {
             AspectJsonSchemaProvider(project),
             BoardJsonSchemaProvider(project),
             BpmnKpiSettingsJsonSchemaProvider(project),
+            BpmnTaskAttsSyncJsonSchemaProvider(project),
             EndpointJsonSchemaProvider(project),
             JournalJsonSchemaProvider(project),
             ModelTypeJsonSchemaProvider(project),
